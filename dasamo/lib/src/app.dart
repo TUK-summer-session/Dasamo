fb3bbe7783fd894eba1c50f2eebf9b44ee58a3a7
1-yuna <micha0215@naver.com>
2024-07-18 20:39:40 +0900

    초기세팅

---
+++ dasamo/lib/src/app.dart
@@ -0,0 +1,76 @@
+import 'package:dasamo/src/screen/community_page.dart';
+import 'package:dasamo/src/screen/home_page.dart';
+import 'package:dasamo/src/screen/my_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  final List<BottomNavigationBarItem> myTabs = [
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      label: '홈',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.feed),
+      label: '동네',
+    ),
+    const BottomNavigationBarItem(
+      icon: Icon(Icons.person_outline),
+      label: '마이',
+    ),
+  ];
+
+  final List<Widget> myTabItems = [
+    HomePage(),
+    CommunityPage(),
+    MyPage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        items: myTabs,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: myTabItems,
+      ),
+    );
+  }
+}
