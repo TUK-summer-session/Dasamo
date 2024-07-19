fc1925e4bb82960ed21f1e1ec85e0114241dddc1
1-yuna <micha0215@naver.com>
2024-07-20 01:51:33 +0900

    appbar 등 기타사항 수정

--- dasamo/lib/src/screens/community_page.dart
+++ dasamo/lib/src/screens/community_page.dart
@@ -1,12 +1,10 @@
-// 홈 페이지 위젯
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:dasamo/src/controllers/community_controller.dart';
 import 'package:dasamo/src/widgets/Input/comment_input.dart';
 import 'package:dasamo/src/widgets/Input/comments.dart';
 import 'package:dasamo/src/widgets/expand/expand_text.dart';
-import 'package:dasamo/src/widgets/list/star_list_widget.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-// 리뷰 세부페이지
 class CommunityPage extends StatefulWidget {
   const CommunityPage({super.key});
 
@@ -22,214 +20,239 @@ class _CommunityPageState extends State<CommunityPage> {
   bool _commentHovered = false;
   bool _commentTapped = false;
 
-  bool _bookmarkHovered = false;
-  bool _bookmarkTapped = false;
-
-  // 구매하기 버튼 hover
-  bool _buttonHovered = false;
-
-  // 텍스트 확장
-  bool _isExpanded = false;
+  final CommunityController communityController =
+      Get.put(CommunityController());
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start, // 세로방향 왼쪽
-        crossAxisAlignment: CrossAxisAlignment.start, // 가로방향 왼쪽
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 20, 20, 0), // 원하는 패딩 값 설정
-            child: Text('커뮤니티'),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        leading: SizedBox.shrink(),
+        leadingWidth: 0,
+        title: const Text('커뮤니티'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.notifications_none_outlined),
           ),
-          // 프로필
-          Container(
-            padding: EdgeInsets.all(20.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment:
-                  MainAxisAlignment.spaceBetween, // 첫 번째와 마지막을 양쪽 끝에 정렬
-              children: <Widget>[
-                Row(children: <Widget>[
+        ],
+      ),
+      body: Obx(() {
+        return SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: communityController.communityList.map((comment) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // 프로필
                   Container(
-                    width: 70,
-                    height: 70,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        image:
-                            AssetImage('assets/images/profile.jpg'), // 로컬 이미지
-                        fit: BoxFit.cover, // 이미지를 컨테이너 크기에 맞춤
-                      ),
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(left: 20), // 왼쪽 여백 추가
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('작성자'),
-                        SizedBox(height: 5),
-                        Text(
-                          '게시일',
-                          style: TextStyle(
-                            fontSize: 10,
-                            color: Colors.grey,
-                          ),
+                    padding: EdgeInsets.all(20.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              width: 70,
+                              height: 70,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  image: AssetImage(comment['profileImage']),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(left: 20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(comment['author']),
+                                  SizedBox(height: 5),
+                                  Text(
+                                    comment['date'],
+                                    style: TextStyle(
+                                      fontSize: 10,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
                       ],
                     ),
                   ),
-                ]),
-              ],
-            ),
-          ),
 
-          // 리뷰 이미지
-          Container(
-            height: 300,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/salad.jpg'), // 로컬 이미지
-                fit: BoxFit.cover, // 이미지를 컨테이너 크기에 맞춤
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1), // 그림자 색상과 투명도
-                  spreadRadius: 0.5, // 그림자 크기
-                  blurRadius: 3, // 흐림 정도
-                  offset: Offset(0, 3), // 그림자 위치 (x, y)
-                ),
-              ],
-            ),
-          ),
-
-          // 하트, 댓글 등
-          Container(
-            padding: EdgeInsets.fromLTRB(20.0, 20, 20, 20),
-            child: Row(
-              mainAxisAlignment:
-                  MainAxisAlignment.spaceBetween, // 첫 번째와 마지막을 양쪽 끝에 정렬
-              children: <Widget>[
-                Row(children: <Widget>[
-                  InkWell(
-                    onHover: (hovered) {
-                      setState(() {
-                        _favoriteHovered = hovered; // 마우스 호버 상태 업데이트
-                      });
-                    },
-                    onTap: () {
-                      setState(() {
-                        _favoriteTapped = !_favoriteTapped; // 아이콘의 탭 상태 토글
-                      });
-                      print('Bookmark 아이콘을 탭했습니다.');
-                    },
-                    child: _favoriteTapped
-                        ? Icon(
-                            Icons.favorite,
-                            color: Colors.red, // 클릭된 경우 색상
-                          )
-                        : Icon(
-                            _favoriteHovered
-                                ? Icons.favorite
-                                : Icons.favorite_border,
-                            color: _favoriteHovered ? Colors.red : null,
-                          ),
-                  ),
-                  SizedBox(width: 5),
-                  InkWell(
-                    onHover: (hovered) {
-                      setState(() {
-                        _commentHovered = hovered;
-                      });
-                    },
-                    onTap: () {
-                      showModalBottomSheet(
-                        context: context,
-                        isScrollControlled: true, // 모달 높이 조절
-                        builder: (context) {
-                          double screenHeight =
-                              MediaQuery.of(context).size.height;
-                          return Container(
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(30),
-                              ),
-                              height: screenHeight * 0.7,
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Expanded(
-                                    child: SingleChildScrollView(
-                                      child: Column(
-                                        children: [
-                                          Container(
-                                            height: 40,
-                                            alignment: Alignment.center,
-                                            child: Container(
-                                              // 내부 컨테이너 예시
-                                              height: 5,
-                                              width: 50,
-                                              decoration: BoxDecoration(
-                                                color: Colors.black,
-                                                borderRadius:
-                                                    BorderRadius.circular(30),
-                                              ),
-                                            ),
-                                          ), // 간격을 주기 위한 Container
-                                          Comments(),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(20.0),
-                                    child: CommentInput(
-                                      onSave: (comment) {
-                                        // 저장 로직을 이곳에 구현
-                                        print('저장된 댓글: $comment');
-                                        // 저장 처리 로직 추가 가능
-                                      },
-                                    ),
-                                  ),
-                                ],
-                              ));
-                        },
-                      );
-                      print('Comment 아이콘을 탭했습니다.');
-                    },
-                    child: Icon(
-                      _commentHovered
-                          ? Icons.chat_bubble
-                          : Icons.chat_bubble_outline,
-                      color: _commentHovered
-                          ? Color.fromRGBO(175, 99, 120, 1.0)
-                          : null,
+                  // 리뷰 이미지
+                  Container(
+                    height: 300,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(comment['image']),
+                        fit: BoxFit.cover,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          spreadRadius: 0.5,
+                          blurRadius: 3,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
                     ),
                   ),
-                ]),
-              ],
-            ),
-          ),
 
-          Container(
-            padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              ExpandText(
-                text:
-                    '샬라샬라샤랄랴살샤ㅏㄹㄹ샤ㅏㄹ샬샬라샬라샬라샤랄랴살샤ㅏㄹㄹ샤ㅏㄹ샬샬라샬라샬라샤랄랴살샤ㅏㄹㄹ샤ㅏㄹ샬샬라샬라샬라샤랄랴살샤ㅏㄹㄹ샤ㅏㄹ샬샬라샬라샬라샤랄랴살샤ㅏㄹㄹ샤ㅏㄹ샬샬라샬라샬라샤랄랴살샤ㅏㄹㄹ샤ㅏㄹ샬샬라샬라샬라샤랄랴살샤ㅏㄹㄹ샤ㅏㄹ샬샬라샬라샬라샤랄랴살샤ㅏㄹㄹ샤ㅏㄹ샬샬라',
-                style: TextStyle(
-                  fontSize: 15, // 글자 크기
-                ),
-              ),
-              SizedBox(height: 10),
-            ]),
+                  // 하트, 댓글 등
+                  Container(
+                    padding: EdgeInsets.fromLTRB(20.0, 20, 20, 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            InkWell(
+                              onHover: (hovered) {
+                                setState(() {
+                                  _favoriteHovered = hovered;
+                                });
+                              },
+                              onTap: () {
+                                setState(() {
+                                  _favoriteTapped = !_favoriteTapped;
+                                });
+                                print('Bookmark 아이콘을 탭했습니다.');
+                              },
+                              child: _favoriteTapped
+                                  ? Icon(
+                                      Icons.favorite,
+                                      color: Colors.red,
+                                    )
+                                  : Icon(
+                                      _favoriteHovered
+                                          ? Icons.favorite
+                                          : Icons.favorite_border,
+                                      color:
+                                          _favoriteHovered ? Colors.red : null,
+                                    ),
+                            ),
+                            SizedBox(width: 5),
+                            InkWell(
+                              onHover: (hovered) {
+                                setState(() {
+                                  _commentHovered = hovered;
+                                });
+                              },
+                              onTap: () {
+                                showModalBottomSheet(
+                                  context: context,
+                                  isScrollControlled: true,
+                                  builder: (context) {
+                                    double screenHeight =
+                                        MediaQuery.of(context).size.height;
+                                    return Container(
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(30),
+                                      ),
+                                      height: screenHeight * 0.7,
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Expanded(
+                                            child: SingleChildScrollView(
+                                              child: Column(
+                                                children: [
+                                                  Container(
+                                                    height: 40,
+                                                    alignment: Alignment.center,
+                                                    child: Container(
+                                                      height: 5,
+                                                      width: 50,
+                                                      decoration: BoxDecoration(
+                                                        color: Colors.black,
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(30),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  Comments(), // Comments widget
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.all(20.0),
+                                            child: CommentInput(
+                                              onSave: (comment) {
+                                                communityController.addComment({
+                                                  'profileImage':
+                                                      'assets/images/profile_new.jpg',
+                                                  'author': 'New User',
+                                                  'date': '2024-07-21',
+                                                  'image':
+                                                      'assets/images/salad.jpg',
+                                                  'text': comment,
+                                                });
+                                                print('저장된 댓글: $comment');
+                                              },
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                );
+                                print('Comment 아이콘을 탭했습니다.');
+                              },
+                              child: Icon(
+                                _commentHovered
+                                    ? Icons.chat_bubble
+                                    : Icons.chat_bubble_outline,
+                                color: _commentHovered
+                                    ? Color.fromRGBO(175, 99, 120, 1.0)
+                                    : null,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // 확장 텍스트
+                  Container(
+                    padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ExpandText(
+                          text: comment['text'],
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            }).toList(),
           ),
-        ],
-      ),
+        );
+      }),
     );
   }
 }
--- dasamo/lib/src/widgets/Input/comments.dart
+++ dasamo/lib/src/widgets/Input/comments.dart
@@ -1,72 +1,50 @@
+import 'package:dasamo/src/controllers/comments_contoller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Comments extends StatelessWidget {
-  final List<Map<String, dynamic>> comments = [
-    {
-      'profileImage': 'assets/images/salad.jpg',
-      'author': 'User1',
-      'content': '이 글 정말 좋네요!',
-    },
-    {
-      'profileImage': 'assets/images/salad.jpg',
-      'author': 'User2',
-      'content': '감사합니다!',
-    },
-    {
-      'profileImage': 'assets/images/salad.jpg',
-      'author': 'User2',
-      'content': '감사합니다!',
-    },
-    {
-      'profileImage': 'assets/images/salad.jpg',
-      'author': 'User2',
-      'content': '감사합니다!',
-    },
-    {
-      'profileImage': 'assets/images/salad.jpg',
-      'author': 'User2',
-      'content': '감사합니다!',
-    },
-  ];
+  final CommentsController commentsController = Get.put(CommentsController());
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: comments.map((comment) {
-        return Padding(
-          padding: const EdgeInsets.fromLTRB(30.0, 20, 30, 0), // 패딩값 설정
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        image: AssetImage(comment['profileImage']),
-                        fit: BoxFit.cover,
+    return Obx(() {
+      return Column(
+        children: commentsController.commentsList.map((comment) {
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(30.0, 20, 30, 0), // 패딩값 설정
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: AssetImage(comment['profileImage']),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  Text(comment['author']),
-                ],
-              ),
-              SizedBox(width: 20),
-              Expanded(
-                child: Text(
-                  comment['content'],
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
+                    SizedBox(height: 5),
+                    Text(comment['author']),
+                  ],
                 ),
-              ),
-            ],
-          ),
-        );
-      }).toList(),
-    );
+                SizedBox(width: 20),
+                Expanded(
+                  child: Text(
+                    comment['content'],
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
+                ),
+              ],
+            ),
+          );
+        }).toList(),
+      );
+    });
   }
 }
