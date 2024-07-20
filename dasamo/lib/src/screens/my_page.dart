1b7fc7b2dc5706589474218e0ee96b321de27613
1-yuna <micha0215@naver.com>
2024-07-20 23:37:53 +0900

    마이페이지 코드정리 , 커뮤니티 버튼 추가

--- dasamo/lib/src/screens/my_page.dart
+++ dasamo/lib/src/screens/my_page.dart
@@ -1,3 +1,5 @@
+import 'package:dasamo/src/controllers/my_page_tab_controller.dart';
+import 'package:dasamo/src/shared/tab_data.dart';
 import 'package:flutter/material.dart';
 
 class MyPage extends StatefulWidget {
@@ -8,26 +10,7 @@ class MyPage extends StatefulWidget {
 }
 
 class _MyPageState extends State<MyPage> {
-  int _selectedIndex = 0; // 현재 선택된 인덱스
-
-  // 각 탭에 따라 보여줄 이미지를 설정합니다.
-  final List<List<String>> _tabImages = [
-    [
-      'assets/images/salad.jpg',
-      'assets/images/salad.jpg',
-      'assets/images/salad.jpg'
-    ],
-    [
-      'assets/images/profile.jpg',
-      'assets/images/profile.jpg',
-      'assets/images/profile.jpg'
-    ],
-    [
-      'assets/images/product.jpg',
-      'assets/images/product.jpg',
-      'assets/images/product.jpg'
-    ],
-  ];
+  final MyPageTabController controller = MyPageTabController();
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +18,12 @@ class _MyPageState extends State<MyPage> {
     double screenWidth = MediaQuery.of(context).size.width;
 
     // 바의 위치를 텍스트 항목에 맞게 조정합니다.
-    double barWidth = screenWidth / 3; // 텍스트 항목의 개수에 따라 바의 너비 설정
-    double barPosition = screenWidth / 3 * _selectedIndex;
+    double barWidth =
+        screenWidth / tabDataList.length; // 텍스트 항목의 개수에 따라 바의 너비 설정
+    double barPosition = barWidth * controller.selectedIndex;
+
+    // 현재 선택된 탭의 데이터
+    final currentTabData = tabDataList[controller.selectedIndex];
 
     return Scaffold(
       appBar: AppBar(
@@ -51,120 +38,118 @@ class _MyPageState extends State<MyPage> {
           )
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          // 프로필
-          Container(
-            padding: EdgeInsets.fromLTRB(40, 30, 0, 20),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Row(children: <Widget>[
-                  Container(
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/profile.jpg'),
-                        fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            // 프로필
+            Container(
+              padding: EdgeInsets.fromLTRB(40, 30, 0, 20),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(children: <Widget>[
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/profile.jpg'),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '작성자',
-                          style: TextStyle(
-                            fontSize: 25,
+                    Container(
+                      padding: EdgeInsets.only(left: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '작성자',
+                            style: TextStyle(
+                              fontSize: 25,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
+                    ),
+                  ]),
+                ],
+              ),
+            ),
+
+            // 텍스트 및 바
+            Container(
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: Stack(
+                children: [
+                  // 바
+                  AnimatedPositioned(
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                    left: barPosition,
+                    bottom: 0,
+                    child: Container(
+                      height: 5,
+                      width: barWidth,
+                      color: Color.fromRGBO(175, 99, 120, 1),
                     ),
                   ),
-                ]),
-              ],
+
+                  // 텍스트
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Row(
+                      children: tabDataList.asMap().entries.map((entry) {
+                        return _buildTab(
+                          entry.value.title,
+                          entry.key,
+                        );
+                      }).toList(),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
 
-          // 텍스트 및 바
-          Container(
-            padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-            child: Stack(
-              children: [
-                // 바
-                AnimatedPositioned(
-                  duration: Duration(milliseconds: 300),
-                  curve: Curves.easeInOut,
-                  left: barPosition,
-                  bottom: 0,
-                  child: Container(
-                    height: 5,
-                    width: barWidth,
-                    color: Color.fromRGBO(175, 99, 120, 1),
-                  ),
-                ),
-
-                // 텍스트
-                Padding(
-                  padding: EdgeInsets.only(bottom: 10),
-                  child: Row(
-                    children: [
-                      _buildTab('내 리뷰', 0),
-                      _buildTab('내 운동', 1),
-                      _buildTab('스크랩', 2),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-
-          // 추가 이미지나 콘텐츠
-          Expanded(
-            child: GridView.builder(
-              // padding: EdgeInsets.all(20),
+            // 추가 이미지나 콘텐츠
+            GridView.builder(
               shrinkWrap: true, // GridView의 크기를 부모에 맞게 조정합니다.
-              physics: NeverScrollableScrollPhysics(), // 스크롤 비활성화
+              physics: NeverScrollableScrollPhysics(), // GridView 자체의 스크롤 비활성화
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 3, // 3개의 열
-                //crossAxisSpacing: 10, // 열 간 간격
-                //mainAxisSpacing: 10, // 행 간 간격
                 childAspectRatio: 1, // 정사각형 모양
               ),
-              itemCount: _tabImages[_selectedIndex].length, // 선택된 탭에 맞는 이미지 개수
+              itemCount: currentTabData.images.length, // 선택된 탭에 맞는 이미지 개수
               itemBuilder: (context, index) {
                 return Container(
                   decoration: BoxDecoration(
                     image: DecorationImage(
-                      image: AssetImage(_tabImages[_selectedIndex]
-                          [index]), // 현재 선택된 탭에 따라 이미지 설정
+                      image: AssetImage(
+                          currentTabData.images[index]), // 현재 선택된 탭에 따라 이미지 설정
                       fit: BoxFit.cover,
                     ),
                   ),
                 );
               },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
   Widget _buildTab(String title, int index) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double tabWidth = screenWidth / 3;
+    double tabWidth = screenWidth / tabDataList.length;
 
     return GestureDetector(
       onTap: () {
         setState(() {
-          _selectedIndex = index;
+          controller.setSelectedIndex(index);
         });
       },
       child: MouseRegion(
@@ -176,9 +161,10 @@ class _MyPageState extends State<MyPage> {
           child: Text(
             title,
             style: TextStyle(
-              fontSize: 25,
-              fontWeight:
-                  _selectedIndex == index ? FontWeight.bold : FontWeight.normal,
+              fontSize: 20,
+              fontWeight: controller.selectedIndex == index
+                  ? FontWeight.bold
+                  : FontWeight.normal,
             ),
           ),
         ),
