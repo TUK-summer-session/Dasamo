d36e14799b9feaeb4826c5e8b7c168c2adbfdcfa
gawon <gawonlee0712@gmail.com>
2024-07-21 15:50:50 +0900

    [feat] 리뷰 홈 api 완료

--- dasamo_server/src/api/review/controller.js
+++ dasamo_server/src/api/review/controller.js
@@ -1,8 +1,41 @@
 const db = require('../../config/dbConfig');
+const createResponse = require('../../utils/response');
 
-exports.index = (req, res) => {
-    res.send('Review home');
+exports.index = async (req, res) => {
+    try {
+        // Review 테이블에서 모든 리뷰 가져옴(임시)
+        const reviews = await db.query('SELECT * FROM Review');
+
+        const data = await Promise.all(reviews.map(async review => {
+            const imageUrlResult = await db.query('SELECT url FROM ReviewImage WHERE `order` = 1 AND ReviewImage.reviewId = ?', [review.reviewId]);
+            let imageUrl = null;
+            if (imageUrlResult.length > 0) {
+                imageUrl = imageUrlResult[0].url;
+            }
+            const tags = await db.query('SELECT name FROM Tag JOIN SelectedTag ON Tag.tagId = SelectedTag.tagId WHERE SelectedTag.reviewId = ?', [review.reviewId]);
+            const tagNames = tags.map((tag, index) => tag.name + (index < tags.length - 1 ? '/' : '')).join(''); // 마지막 태그는 / 삭제
+            const likeCount = await db.query('SELECT COUNT(*) AS count FROM `Like` WHERE feedId = ? AND state = 1', [review.reviewId]);
+            const questionCount = await db.query('SELECT COUNT(*) AS count FROM Scrab WHERE feedId = ? AND state = 1', [review.reviewId]);
+
+            return {
+                reviewId: review.reviewId,
+                title: review.title,
+                detail: review.detail.substring(0, 20),  // 미리보기용 20자로 자르기
+                imageUrl: imageUrl,
+                likeCount: likeCount[0].count,
+                questionCount: questionCount[0].count,
+                tags: tagNames
+            };
+        }));
+
+        const response = createResponse(200, '요청이 성공적으로 처리되었습니다.', { reviews: data });
+        res.send(response);
+    } catch (error) {
+        console.error('Query error:', error);
+        res.status(500).send('서버 오류가 발생했습니다.');
+    }
 };
+
 
 exports.products = (req, res) => {
     res.send('Product list');
