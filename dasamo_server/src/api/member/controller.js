c669429536eaeb8277162a9e845359ba0da517be
gawon <gawonlee0712@gmail.com>
2024-07-23 19:43:28 +0900

    [fix] 회원가입 시 memberId int로 나오게 수정

--- dasamo_server/src/api/member/controller.js
+++ dasamo_server/src/api/member/controller.js
@@ -3,32 +3,34 @@ const createResponse = require('../../utils/response');
 const repository = require('./repository');
 
 exports.login = async (req, res) => {
-    const { id, nickname, email, profileImageUrl} = req.body;
+    const { id, nickname, email, profileImageUrl } = req.body;
     try {
 
-      console.log(`ID: ${id}`);
-      console.log(`Nickname: ${nickname}`);
-      console.log(`Email: ${email}`);
-      console.log(`ProfileImage: ${profileImageUrl}`);
+        console.log(`ID: ${id}`);
+        console.log(`Nickname: ${nickname}`);
+        console.log(`Email: ${email}`);
+        console.log(`ProfileImage: ${profileImageUrl}`);
 
-      // db에 이메일 일치하는 놈 있으면 memberId 리턴
-      let memberResult = await repository.getMemberByEmail(email);
-      let member = memberResult[0];
-      if (memberResult.length < 1) {
-          // 없으면 새로 저장해서 memberId 넣어줌
-          const result = await db.query(
-              'INSERT INTO Member (email, name, profileImageUrl) VALUES (?, ?, ?)',
-              [email, nickname, profileImageUrl]
-          );
-          member = { memberId: result.insertId, email, nickname, profileImageUrl };
-      }
-      // TODO: 토큰 인증은 마지막의 마지막에 구현
+        // db에 이메일 일치하는 놈 있으면 memberId 리턴
+        let memberResult = await repository.getMemberByEmail(email); 
+        let member = memberResult[0];
+        if (memberResult.length < 1) {
+            // 없으면 새로 저장해서 memberId 넣어줌
+            const result = await db.query(
+                'INSERT INTO Member (email, name, profileImageUrl) VALUES (?, ?, ?)',
+                [email, nickname, profileImageUrl]
+            );
+            // 데이터베이스 삽입 결과로부터 insertId를 정수로 변환하여 memberId에 할당
+            const memberId = parseInt(result.insertId, 10);
+            member = { memberId, email, nickname, profileImageUrl };
+        }
+        // TODO: 토큰 인증은 마지막의 마지막에 구현
 
-      const response = createResponse(200, '요청이 성공적으로 처리되었습니다.', {member});
-      res.send(response);
+        const response = createResponse(200, '요청이 성공적으로 처리되었습니다.', { member });
+        res.send(response);
     } catch (error) {
-      console.error('카카오 사용자 정보 요청 실패', error);
-      res.status(500).send('서버 오류가 발생했습니다.');
+        console.error('카카오 사용자 정보 요청 실패', error);
+        res.status(500).send('서버 오류가 발생했습니다.');
     }
 };
 
